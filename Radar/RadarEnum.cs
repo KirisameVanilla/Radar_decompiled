b34216e61101bb1239ad637f572de344967fff69
KirisameVanilla <vanillaaaajp@gmail.com>
2024-11-10 15:07:03 +0800

    refactor: indent

--- Radar/RadarEnum.cs
+++ Radar/RadarEnum.cs
@@ -3,64 +3,64 @@ namespace Radar;
 public static class RadarEnum
 {
     public enum MyObjectKind
-{
-	None,
-	Pet,
-	Chocobo,
-	Player,
-	BattleNpc,
-	EventNpc,
-	Treasure,
-	Aetheryte,
-	GatheringPoint,
-	EventObj,
-	MountType,
-	Companion,
-	Retainer,
-	Area,
-	Housing,
-	Cutscene,
-	CardStand,
-	Unknown
-}
+    {
+        None,
+        Pet,
+        Chocobo,
+        Player,
+        BattleNpc,
+        EventNpc,
+        Treasure,
+        Aetheryte,
+        GatheringPoint,
+        EventObj,
+        MountType,
+        Companion,
+        Retainer,
+        Area,
+        Housing,
+        Cutscene,
+        CardStand,
+        Unknown
+    }
 
-public enum MapMode
-{ 
-    Free,
-    Snap,
-    SnapRotate
-}
+    public enum MapMode
+    {
+        Free,
+        Snap,
+        SnapRotate
+    }
 
-public enum DetailLevel
-{
-    仅图标,
-    仅物体名,
-    物体名距离,
-    详细信息
-}
+    public enum DetailLevel
+    {
+        仅图标,
+        仅物体名,
+        物体名距离,
+        详细信息
+    }
 
-public enum RingSegmentsType
-{
-    Quad = 4,
-    Hexagon = 6,
-    Circle = 0
-}
+    public enum RingSegmentsType
+    {
+        Quad = 4,
+        Hexagon = 6,
+        Circle = 0
+    }
 
-public enum DeepDungeonType
-{
-    Trap,
-    AccursedHoard,
-    Wall,
-    Room,
-    Mob,
-    CairnOfReturn,
-    CairnOfPassage
-}
+    public enum DeepDungeonType
+    {
+        Trap,
+        AccursedHoard,
+        Wall,
+        Room,
+        Mob,
+        CairnOfReturn,
+        CairnOfPassage
+    }
 
-public enum SubKind : byte
-{
-    Pet = 2,
-    Chocobo,
-}
+    public enum SubKind : byte
+    {
+        Pet = 2,
+        Chocobo,
+    }
 }
 
